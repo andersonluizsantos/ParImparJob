deabf1e07d22856bbebc018a673eb60b5ec2c297
Anderson Santos <andersonluizsantos@gmail.com>
2021-12-29 14:55:59 -0300

    Criação de um step Chunk-based

--- src/main/java/com/udemy/parimparjob/ParImparBatchConfig.java
+++ src/main/java/com/udemy/parimparjob/ParImparBatchConfig.java
@@ -1,11 +1,19 @@
 package com.udemy.parimparjob;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +33,29 @@ public class ParImparBatchConfig {
 	}
 
 	public Step imprimeParImparStep() {
-		//TODO: Implementar o step baseado em chunk
-		return null;
+		return stepBuilderFactory
+				.get("imprimeParImparStep")
+				.<Integer, String>chunk(1)
+				.reader(contaAteDezReader())
+				.processor(parOuImparProcessor())
+				.writer(imprimeWriter())
+				.build();
 	}
+
+	private ItemWriter<String> imprimeWriter() {
+		return itens -> itens.forEach(System.out::println);
+	}
+
+	private FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+		return new FunctionItemProcessor<Integer, String>
+		(item -> item %2 == 0 ? String.format("Item %s é par ", item) : String.format("Item %s é impar ", item));
+	}
+
+	private IteratorItemReader<Integer> contaAteDezReader() {
+		List<Integer> numerosAteDez = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		return new IteratorItemReader<Integer>(numerosAteDez.iterator());
+	}
+	
+	
+	
 }
